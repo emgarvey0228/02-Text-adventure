bfad7ae1d6c2f4e0cc2661c80e9ee43de82ea4db
Emily Garvey <54558528+emgarvey@users.noreply.github.com>
2020-02-02 17:17:19 -0500

    23:12 on youtube

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ assert sys.version_info >= (3,7), "This script requires at least Python 3.7"
 # The game and item description files (in the same folder as this script)
 game_file = 'zork.json'
 item_file = 'items.json'
-
+inventory = [""]
+points = 0
+moves = 0
 
 # Load the contents of the files into the game and items dictionaries. You can largely ignore this
 # Sorry it's messy, I'm trying to account for any potential craziness with the file location
@@ -22,21 +24,56 @@ def load_files():
 
 
 
-def render (game,items,current):
+
+def check_inventory(item):
+    for i in inventory:
+        if i == item:
+            return True
+        return False
+
+
+def render (game,items,current,moves,points):
     c = game [current]
-    print("You are at the " + c["name"])
+    print ("\n\n{} Moves\t{} Points".format(moves,points))
+    print("\n\nYou are at the " + c["name"])
     print(c["desc"])
+
+    #display any items
+    for i in c ["items"]:
+        if not check_inventory(i("item")):
+            print(i["desc"])
+
 
 def get_input():
     response = input ("What would you like to do?")
     response = response.upper().strip()
     return response
 
+
+
+
 def update(game,items,current,response):
+    if response == "INVENTORY":
+        print ("You are carrying:")
+        if not len(inventory) == 0:
+            print ("Nothing")
+        else:
+            for i in inventory:
+                print(i.lower())
+        return current
+    
     c = game [current]
     for e in c["exits"]:
         if response == e["exit"]:
+            moves += 1
             return e["target"]
+    
+    for i in c ["items"]:
+        if response == "GET" + i["item"] and not check_inventory (i["item"]):
+            print (i["take"])
+            inventory.append(i["item"])
+            return current
+     
     return current
 
 
@@ -50,6 +87,7 @@ def main():
     (game,items) = load_files()
 
     
+    
     while True:
         render(game,items,current)
         response = get_input()
@@ -59,8 +97,8 @@ def main():
 
         current = update (game,items,current,response)
         
-
-
+    print("Thanks for playing!")
+    print ("You scored {} points in {} moves".format(points,moves))
 # run the main function
 if __name__ == '__main__':
 	main()

313ba70f22a3e6fb94ff20c690bb0d454d448f08
Emily Garvey <54558528+emgarvey@users.noreply.github.com>
2020-02-03 22:13:32 -0500

    updated

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ assert sys.version_info >= (3,7), "This script requires at least Python 3.7"
 
 # The game and item description files (in the same folder as this script)
 game_file = 'game.json'
-item_file = 'items.json'
-inventory = []
-
-
+item_file = 'item.json'
+inventory = ['']
+points = 0
+moves = 0
 
 # Load the contents of the files into the game and items dictionaries. You can largely ignore this
 # Sorry it's messy, I'm trying to account for any potential craziness with the file location
@@ -23,31 +23,31 @@ def load_files():
         os._exit(1)
 
 
-def check_inventory(item):
+def check_inventory(items):
     for i in inventory:
-        if i == item:
+        if i == items:
             return True
-    return False
+    return False 
 
 def calculate_points(items):
     points = 0
     for i in inventory:
         if i in items:
             points += items[i]["points"]
-    return points
+    return points 
 
-def render (game,items,current,moves,points):
-    c = game [current]
-    print ("\n\n{} Moves\t\t\t\t{} Points".format(moves,points))
+
+def render(game,items,current,moves,points):
+    c = game[current]
+    print("\n\n{} Moves\t\t\t\t{} Points".format(moves, points))
     print("\n\nYou are at the " + c["name"])
     print(c["desc"])
 
     #display any items
-    for item in c ["items"]:
-        if not check_inventory(item("item")):
+    for item in c["items"]:
+        if not check_inventory(item):
             print(item["desc"])
-    
-    
+
     #display item information
     for i in inventory:
         if i in items:
@@ -59,31 +59,33 @@ def render (game,items,current,moves,points):
         print(e["exit"].lower())
 
 def get_input():
-    response = input ("What would you like to do?")
+    response = input("\nWhat do you want to do? ")
     response = response.upper().strip()
     return response
 
 
 
 def update(game,items,current,response):
+
     if response == "INVENTORY":
-        print ("You are carrying:")
+        print("You are carrying:")
         if len(inventory) == 0:
-            print ("Nothing")
+            print("Nothing")
         else:
             for i in inventory:
                 print(i.lower())
         return current
-    
-    c = game [current]
+
+    c = game[current]
     for e in c["exits"]:
         if response == e["exit"]:
             return e["target"]
-    
-    for i in c ["items"]:
-        if response == "GET" + item["item"] and not check_inventory (item["item"]):
-            print (item["take"])
-            inventory.append(item["item"])
+        return current
+
+    for i in c["items"]:
+        if response == "GET" or "TAKE" or "PICK UP" + items["items"] and not check_inventory(items["items"]):
+            print(items["take"])
+            inventory.append(items["items"])
             return current
     
     for i in inventory:
@@ -92,48 +94,45 @@ def update(game,items,current,response):
                 if response == action + " " + i:
                     print(items[i]["actions"][action])
                     return current
-        
-    
-    if response[0:3] == "GET":
-        print("You can't take that!")
-    elif response in ["NORTH","SOUTH","EAST","WEST","NORTHWEST","NORTHEAST","SOUTHWEST","SOUTHEAST","UP","DOWN"]:
-        print("You can't go that way!")
-    else:
-        print("I don't understand what you're trying to do")
-     
-    return current
 
+    if response[0:3] == "GET" or "TAKE" or "PICK UP":
+        print("You can't take that.")
+    elif response in ["NOTH","SOUTH","EAST","WEST","NW","NE","SE","SW","UP","DOWN"]:
+        print("You can't go that way.")
+    else: 
+        print("I don't understand what you're trying to do.")
+
+    return current
+    
 
 
 
 # The main function for the game
 def main():
-    current = 'IMU'  # The starting location
-    end_game = ['END']  # Any of the end-game locations
-    moves = 0
+    current = 'START'  # The starting location
+    end_game = ['BLACK']  # Any of the end-game locations
     points = 0
+    moves = 0
 
     (game,items) = load_files()
 
-    
-    
     while True:
         render(game,items,current,moves,points)
         if current in end_game:
             break
 
         response = get_input()
-        
-        if response == "QUIT":
-            break
 
-        current = update (game,items,current,response)
+        if response == "QUIT" or response == "Q":
+            break #break out of the while loop
+
+        current = update(game,items,current,response)
         moves += 1
-        points = calculate_points()
-        
-    print("Thanks for playing!")
-    print ("You scored {} points in {} moves".format(points,moves))
+        points = calculate_points(items)
+
+    print("I hope you enjoyed playing! See you next time.")
+    print("You scored {} points in {} moves.".format(points,moves))
 
 # run the main function
 if __name__ == '__main__':
-	main()
+    main()
